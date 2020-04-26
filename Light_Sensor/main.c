399f9d8e6b1122e2ce935d51ebb3d4f883b0ecc3
Tao173 <64321391+Tao173@users.noreply.github.com>
2020-04-26 13:42:14 +0800

    Initial commit

---
+++ Light_Sensor/main.c
@@ -0,0 +1,113 @@
+/*
+Copyright 2019 Achim Pieters | StudioPieters®
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <espressif/esp_common.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include <wifi_config.h>
+
+const int led_gpio = 2;
+bool led_on = false;
+
+void led_write(bool on) {
+        gpio_write(led_gpio, on ? 0 : 1);
+}
+void identify_task(void *_args) {
+        for (int i=0; i<3; i++) {
+                for (int j=0; j<2; j++) {
+                        led_write(true);
+                        vTaskDelay(100 / portTICK_PERIOD_MS);
+                        led_write(false);
+                        vTaskDelay(100 / portTICK_PERIOD_MS);
+                }
+                vTaskDelay(250 / portTICK_PERIOD_MS);
+        }
+        led_write(led_on);
+        vTaskDelete(NULL);
+}
+void identify(homekit_value_t _value) {
+        printf("Identify\n");
+        xTaskCreate(identify_task, "Identify", 128, NULL, 2, NULL);
+}
+homekit_characteristic_t currentAmbientLightLevel = HOMEKIT_CHARACTERISTIC_(CURRENT_AMBIENT_LIGHT_LEVEL, 0,.min_value = (float[]) {0},);
+
+
+void light_sensor_task(void *_args) {
+
+        uint16_t analog_light_value;
+        while (1) {
+          // The code below reads the LDR (Light Dependent Resistor) on the Analog PGIO from the ESP unit.
+          // As HomeKit uses Lux this isn't a correct reading. This code gives a demonstration a this sensor type.
+          // If you know a different approach, feel free to share with me and the communnity.
+                analog_light_value = sdk_system_adc_read();
+                currentAmbientLightLevel.value.float_value = (1*100/(1000-analog_light_value));
+                homekit_characteristic_notify(&currentAmbientLightLevel, HOMEKIT_FLOAT((analog_light_value)));
+                vTaskDelay(3000 / portTICK_PERIOD_MS);
+        }
+}
+void light_sensor_init() {
+        xTaskCreate(light_sensor_task, "Light Sensor", 256, NULL, 2, NULL);
+}
+
+
+homekit_accessory_t *accessories[] = {
+        HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]){
+                HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+                        HOMEKIT_CHARACTERISTIC(NAME, "Light Sensor"),
+                        HOMEKIT_CHARACTERISTIC(MANUFACTURER, "StudioPieters®"),
+                        HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "Q39QDPS8GRX8"),
+                        HOMEKIT_CHARACTERISTIC(MODEL, "HKVF2T/L"),
+                        HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.0.1"),
+                        HOMEKIT_CHARACTERISTIC(IDENTIFY, identify),
+                        NULL
+                }),
+                HOMEKIT_SERVICE(LIGHT_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+                        HOMEKIT_CHARACTERISTIC(NAME, "Light Sensor"),
+                        &currentAmbientLightLevel,
+                        NULL
+                }),
+                NULL
+        }),
+        NULL
+};
+homekit_server_config_t config = {
+        .accessories = accessories,
+        .password = "595-33-595",
+        .setupId="RW5X",
+};
+//accessory category code = 10
+void user_init(void) {
+        uart_set_baud(0, 115200);
+
+        light_sensor_init();
+        void on_wifi_ready();
+
+        homekit_server_init(&config);
+}
